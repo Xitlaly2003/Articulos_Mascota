481d6267df688b873a84d6a240b1888c43c90129
AnaVel2002 <Anavel.2002@proton.me>
2024-11-26 20:09:40 -0600

    Añadi el archivo para la conección BD

---
+++ app/config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'integrador';
+
+$conn = new mysqli($host, $user, $password, $db);
+
+// Verificar si hubo error en la conexión
+if ($conn->connect_error) {
+    die("Error al conectar con la base de datos: " . $conn->connect_error);
+}
+
+// Retornar la conexión
+return $conn;
+?>
